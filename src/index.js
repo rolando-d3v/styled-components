dd29da995e9431223ab0bc102a715e856ae606eb
raidentek <33987212+raidentek@users.noreply.github.com>
2021-03-09 22:13:11 -0500

    red

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GlobalStyle from './styles/globalStyled';
 
 
 ReactDOM.render(
   <React.Fragment>
+    <GlobalStyle />
     <App />
   </React.Fragment>,
   document.getElementById('root')
