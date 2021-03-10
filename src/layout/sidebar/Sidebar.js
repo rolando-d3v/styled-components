4b16b8e36af74e94e5fe9ecb47520ff3454c3aed
raidentek <33987212+raidentek@users.noreply.github.com>
2021-03-10 06:45:34 -0500

    red

--- src/layout/sidebar/Sidebar.js
+++ src/layout/sidebar/Sidebar.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Sidebar() {
     return (
-        <div>
+        <SidebarContainer>
             
-        </div>
+        </SidebarContainer>
     )
 }
